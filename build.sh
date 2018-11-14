c28fef6464a53a947d3c21c0eff7cbb576d4f021
Gal Bashan <galbashan1@gmail.com>
2018-11-14 14:51:14 +0200

    fix(ci): adding option for local build

--- build.sh
+++ build.sh
@@ -1,7 +1,21 @@
-cd instrumentation/ &&
-    mvn clean package &&
-    cd .. &&
-    cp instrumentation/target/instrumentation-1.0-SNAPSHOT.jar epsagon/src/main/resources/agent.jar &&
-    sync &&
-    cd epsagon/ && mvn clean package &&
-    cd ..
+should_sign=$1
+
+if [ $should_sign = "--sign" ]; then
+    cd instrumentation/ &&
+        mvn clean package &&
+        cd .. &&
+        cp instrumentation/target/instrumentation-1.0-SNAPSHOT.jar epsagon/src/main/resources/agent.jar &&
+        sync &&
+        cd epsagon/ &&
+        mvn clean package &&
+        cd ..
+else
+    cd instrumentation/ &&
+        mvn clean install &&
+        cd .. &&
+        cp instrumentation/target/instrumentation-1.0-SNAPSHOT.jar epsagon/src/main/resources/agent.jar &&
+        sync &&
+        cd epsagon/ &&
+        mvn clean install -P local-build &&
+        cd ..
+fi
